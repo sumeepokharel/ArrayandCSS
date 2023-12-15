4b9314cfca8967ede4660215288febce95b94cb0
Sumee Pokharel <sumeepokharel@Sumees-MacBook-Pro.local>
2023-12-15 14:10:01 -0600

    style component map function css array

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import styles from './App.module.css'
+
+const names = ['Alice', 'Bob', 'Charlie', 'David'];
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className={styles.container}>
+      <h2 className={styles.heading}>List of Names:</h2>
+      
+      <ol className={styles.flexContainer}>
+        {names.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ol>
+    </div>
+
+
+  );
 }
 
-export default App
+export default App;
